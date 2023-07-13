50f2cc791a0fa2be593859588c062450eee00151
RukiaKiriduka <tottoko.rusan1133@gmail.com>
2023-07-13 22:13:05 +0900

    編集

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -5,7 +5,7 @@
         <title>Blog</title>
     </head>
     <body>
-        <h1>チーム開発会へようこそ！</h1>
+        <h1>筋トレ管理アプリ</h1>
         <h2>投稿作成</h2>
         <form action="/posts" method="POST">
             @csrf
