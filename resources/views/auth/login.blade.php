33e3698328677cbcdf52a53e5cef73fd94301cee
RukiaKiriduka <tottoko.rusan1133@gmail.com>
2023-08-23 16:16:26 +0900

    修正

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -33,16 +33,25 @@
                 <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
             </label>
         </div>
-
+　　　　
         <div class="flex items-center justify-end mt-4">
+            <div>
+　　　　     <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('register') }}">
+                {{ __('新規登録') }}
+            </a>
+　　　　</div>
+           
             @if (Route::has('password.request'))
                 <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
                     {{ __('Forgot your password?') }}
                 </a>
             @endif
 
+            
+        </div>
+        <div>
             <x-primary-button class="ml-3">
-                {{ __('Log in') }}
+                {{ __('ログイン') }}
             </x-primary-button>
         </div>
     </form>
